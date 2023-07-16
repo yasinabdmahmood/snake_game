06700aed680bc2da4bfa80f0f6919f61389f4bf3
yasinabdmahmood <abd181103@gmail.com>
2023-07-16 13:55:30 +0300

    Add some empty space for readibility

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ const canvasSize = 500; // Size of the canvas in pixels
 const numRows = gridSize;
 const numColumns = gridSize;
 const snakeBody = [[0,0],[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0],[8,0],[9,0],[10,0],[11,0],[12,0]];
-// 
-// 
+
 
 const game = new Game({numColumns, numRows, snakeBody})
 const squareSize = canvasSize / gridSize; // Size of each square
